ae9cbae34ee446ea8feada5cf4862894eac9b6d3
zigakk03 <zigakk03@gmail.com>
2024-07-30 13:34:39 +0200

    Add QuadFlask Colorpicker

--- app/src/main/java/com/example/timekeeper/NewNotificationFragment.kt
+++ app/src/main/java/com/example/timekeeper/NewNotificationFragment.kt
@@ -1,12 +1,18 @@
 package com.example.timekeeper
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.flask.colorpicker.ColorPickerView
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder
+
 
 class NewNotificationFragment : Fragment() {
 
@@ -14,6 +20,7 @@ class NewNotificationFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +29,29 @@ class NewNotificationFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_new_notification, container, false)
 
         view.findViewById<ImageButton>(R.id.btnSave).setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigate_newNotification_to_home) }
+
+
+        var colorButton = Color.parseColor("#FF0FA2E6")
+        view.findViewById<ImageButton>(R.id.btnColorPicker).setOnClickListener {
+            ColorPickerDialogBuilder
+                .with(context)
+                .setTitle("Choose color")
+                .initialColor(colorButton)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(8)
+                .lightnessSliderOnly()
+                .setPositiveButton(
+                    "ok"
+                ) { dialog, selectedColor, allColors ->
+                    colorButton = selectedColor
+                    view.findViewById<ImageButton>(R.id.btnColorPicker).setBackgroundColor(selectedColor)
+                }
+                .setNegativeButton(
+                    "cancel"
+                ) { dialog, which -> }
+                .build()
+                .show()
+        }
 
         return view
     }
