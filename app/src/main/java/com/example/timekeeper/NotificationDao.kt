61075f72219ff7d23009a1f22074ca9a3001bb6d
zigakk03 <zigakk03@gmail.com>
2024-08-19 15:34:44 +0200

    Update Main Query and Replace App Icon

--- app/src/main/java/com/example/timekeeper/NotificationDao.kt
+++ app/src/main/java/com/example/timekeeper/NotificationDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 import java.sql.Timestamp
+import java.time.LocalDateTime
 
 @Dao
 interface NotificationDao {
@@ -16,13 +17,9 @@ interface NotificationDao {
     @Delete
     suspend fun deleteNotification(notification: Notification)
 
-    // TODO: Implement the correct query
-    /*@Query("SELECT * FROM notification WHERE dateTime <= :currentTime ORDER BY dateTime ASC")
-    fun getRelevantNotifications(currentTime: Timestamp? = Timestamp(System.currentTimeMillis())): List<Notification>
-    */
 
-    @Query("SELECT * FROM notification")
-    suspend fun getRelevantNotifications(): MutableList<Notification>
+    @Query("SELECT * FROM notification WHERE dateTime <= :currentTime ORDER BY dateTime DESC")
+    suspend fun getRelevantNotifications(currentTime: LocalDateTime? = LocalDateTime.now()): MutableList<Notification>
 
     @Query("SELECT * FROM notification WHERE id=:id")
     suspend fun getNotification(id: Int): Notification
